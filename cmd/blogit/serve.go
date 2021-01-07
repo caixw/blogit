8ff863bd2b72ae49072cfb92c1c6dad2ade0a150
caixw <xnotepad@gmail.com>
2021-01-08 01:23:36 +0800

    feat(cmd): 为 serve 添加 -watch 在数

--- cmd/blogit/serve.go
+++ cmd/blogit/serve.go
@@ -4,15 +4,17 @@ package main
 
 import (
 	"io"
+	"log"
 
 	"github.com/caixw/blogit"
 	"github.com/issue9/cmdopt"
 )
 
 var (
-	serveSrc  string
-	serveAddr string
-	servePath string
+	serveSrc   string
+	serveAddr  string
+	servePath  string
+	serveWatch bool
 )
 
 func initServe(opt *cmdopt.CmdOpt) {
@@ -20,8 +22,12 @@ func initServe(opt *cmdopt.CmdOpt) {
 	fs.StringVar(&serveSrc, "src", "./", "指定源码目录")
 	fs.StringVar(&serveAddr, "addr", ":8080", "服务端口")
 	fs.StringVar(&servePath, "path", "/", "根路径")
+	fs.BoolVar(&serveWatch, "watch", false, "监视变化")
 }
 
 func serve(w io.Writer) error {
+	if serveWatch {
+		return blogit.Watch(serveSrc, serveAddr, servePath, log.New(w, "", log.Ldate))
+	}
 	return blogit.Serve(serveSrc, serveAddr, servePath)
 }
