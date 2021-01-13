2aced12dc18ee251f16c2d03cdbdac334a8a8d18
caixw <xnotepad@gmail.com>
2021-01-14 00:49:23 +0800

    fix(internal/loader): MD文件只加载 posts 目录下的

--- internal/loader/post.go
+++ internal/loader/post.go
@@ -76,7 +76,7 @@ type Post struct {
 // LoadPosts 加载所有的文章
 func LoadPosts(dir string) ([]*Post, error) {
 	paths := make([]string, 0, 10)
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(filepath.Join(dir, vars.PostsDir), func(path string, info os.FileInfo, err error) error {
 		if err == nil && !info.IsDir() && strings.ToLower(filepath.Ext(info.Name())) == ".md" {
 			paths = append(paths, path)
 		}
