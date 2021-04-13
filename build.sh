7f63839f2415f0dffc287825d1f2c01d916ad14e
caixw <xnotepad@gmail.com>
2021-04-13 17:30:11 +0800

    ci: 修正打包代码错误

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # SPDX-License-Identifier: MIT
 
-cd $(dirname $0)
+cd $(dirname $0) || exit
 date=$(date -u '+%Y%m%d')
 hash=$(git rev-parse HEAD)
 go build -ldflags "-X github.com/caixw/blogit/internal/vars.metadata=${date}.${hash}" -v -o ./cmd/blogit/blogit ./cmd/blogit
